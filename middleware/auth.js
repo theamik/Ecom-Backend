8cd772b6b11c275c7bd86ce11a6dc3612afb3399
theamik <amik.sylhet@gmail.com>
2023-07-30 09:02:49 +0600

    first commit

---
+++ middleware/auth.js
@@ -0,0 +1,34 @@
+const ErrorHander = require('../utils/errorhander');
+const catchAsyncErrors = require('./catchAsyncErrors');
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+
+exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
+  const { token } = req.cookies;
+  //const token = req.headers.authorization.split(' ')[1];
+
+  if (!token) {
+    return next(new ErrorHander('Please Login to access this resource', 401));
+  }
+
+  const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+
+  req.user = await User.findById(decodedData.id);
+
+  next();
+});
+
+exports.authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorHander(
+          `'${req.user.role}' role is not allowed to access this resource `,
+          403
+        )
+      );
+    }
+
+    next();
+  };
+};
---
+++ server.js
@@ -0,0 +1,36 @@
+const app = require('./app');
+const connectDatabase = require('./config/database');
+// let environment = 'development';
+// // Config
+// if (environment === 'production') {
+//   require('dotenv').config({ path: '.env' });
+// } else {
+//   require('dotenv').config({ path: 'config/config.env' });
+// }
+//require('dotenv').config({ path: 'config/config.env' });
+// console.log(environment);
+
+// Handling Uncaught Exception
+process.on('uncaughtException', (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Uncaught Exception`);
+  process.exit(1);
+});
+//config
+require('dotenv').config({ path: 'config/config.env' });
+// Connecting to database
+connectDatabase();
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
+
+// Unhandled Promise Rejection
+process.on('unhandledRejection', (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
