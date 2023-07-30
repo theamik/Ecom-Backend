3f094c47c6324228855e0011bf39f8a3d55f0d08
theamik <amik.sylhet@gmail.com>
2023-07-30 09:24:21 +0600

    third commit

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ const product = require('./routes/productRoute');
 const user = require('./routes/userRoute');
 const order = require('./routes/orderRoute');
 // const payment = require('./routes/paymentRoute');
-
+app.get('/api/v1',(req,res)=>{
+    res.send("<h1>Working going on</h1>")
+});
 app.use('/api/v1', product);
 app.use('/api/v1', user);
 app.use('/api/v1', order);
