a201081791997e68e3dda82c99c4a797f1e6e135
theamik <amik.sylhet@gmail.com>
2023-08-01 11:34:56 +0600

    6 commit

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const errorMiddleware = require('./middleware/error');
+const cors = require('cors');
 
 // Setting up config file
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
 
